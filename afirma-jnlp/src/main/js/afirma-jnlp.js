95dbd57e23fe23bff993fe9a5b9a98eaffed19c3
Venancio López Martínez <vlopez@farmafene.com>
2015-11-30 10:19:21 +0100

    Arreglos en el cliente

--- afirma-jnlp/src/main/js/afirma-jnlp.js
+++ afirma-jnlp/src/main/js/afirma-jnlp.js
@@ -3,60 +3,40 @@
 //error : function(jqXHR jqXHR,String textStatus, String errorThrown)
 //beforeSend : function(jqXHR jqXHR, PlainObject settings) {
 //complete : function(jqXHR jqXHR,String textStatus) {
-*/
-var MiniAppletJNLP = (function(window, undefined) {
-	var completeCallback;
-	var beforeSendCallback;
-	function setBeforeSendCallback(beforeSendCallback) {
-		MiniAppletJNLP.beforeSendCallback = beforeSendCallback;
-	}
-	function setCompleteCallback(completeCallback) {
-		MiniAppletJNLP.completeCallback = completeCallback;
-	}
-
-	function echo(sucessCallback,errorCallback) {
-		callServer("echo",null,sucessCallback,errorCallback);
-	}
-	function getBase64FromText(/**/ msg,sucessCallback,errorCallback) {
-		callServer("getBase64FromText", msg,sucessCallback,errorCallback);
-	}
-	function sign(/**/ msg,sucessCallback,errorCallback) {
-		callServer("sign", msg,sucessCallback,errorCallback);
-	}
-	
-	function openWindow(sucessCallback,errorCallback) {
-		callServer("open",null,sucessCallback,errorCallback);
-	}
-	function exit_jnlp(sucessCallback,errorCallback) {
-		callServer("exit",null,sucessCallback,errorCallback);
-	}
-	function test(sucessCallback,errorCallback) {
-		callServer("test",null,sucessCallback,errorCallback);
-	}
-	function callServer(/* String */command, /* Any */parameters,
-			successCallback, errorCallback) {
-		var server = "https://localhost:9999/afirma/";
-		console.log("El servidor es: "+server);
-		server = server + command;
-		console.log("El servicio es: "+server);
+ */
+//
+//
+// JAVAScript client
+// 
+var AfirmaClient = function() {
+	this._command;
+	this._successCallback;
+	this._errorCallback;
+	this._beforeSendCallback;
+	this._completeCallback;
+	this._server = "https://localhost:9999/afirma/";
+	this._invoker = function(/* String */command, /* Any */parameters,
+			successCallback, errorCallback, beforeSendCallback,
+			completeCallback) {
+		var urlCommand = this._server + this._command;
 		$.ajax({
-			data : JSON.stringify(parameters,undefined,2),
-			url : server,
+			data : JSON.stringify(parameters, undefined, 2),
+			url : urlCommand,
 			type : 'POST',
 			contentType : 'application/json',
 			crossOrigin : true,
 			dataType : 'json',
 			beforeSend : function(/* jqXHR */jqXHR, /* PlainObject */
 			settings) {
-				if (MiniAppletJNLP.beforeSendCallback) {
-					MiniAppletJNLP.beforeSendCallback(/* jqXHR */jqXHR, /* PlainObject */
+				if (beforeSendCallback) {
+					beforeSendCallback(/* jqXHR */jqXHR, /* PlainObject */
 					settings)
 				}
 			},
 			complete : function( /* jqXHR */jqXHR, /* String */
 			textStatus) {
-				if (MiniAppletJNLP.completeCallback) {
-					MiniAppletJNLP.completeCallback( /* jqXHR */jqXHR, /* String */
+				if (completeCallback) {
+					completeCallback( /* jqXHR */jqXHR, /* String */
 					textStatus)
 				}
 			},
@@ -76,56 +56,28 @@ var MiniAppletJNLP = (function(window, undefined) {
 				}
 			}
 		});
-	}
-
-	return {
-
-	/* Publicamos las variables para la comprobacion de hora. */
-	// CHECKTIME_NO : CHECKTIME_NO,
-	// CHECKTIME_RECOMMENDED : CHECKTIME_RECOMMENDED,
-	// CHECKTIME_OBLIGATORY : CHECKTIME_OBLIGATORY,
-	/*
-	 * Publicamos las variables para establecer parametros personalizados en la
-	 * JVM
-	 */
-	// JAVA_ARGUMENTS : JAVA_ARGUMENTS,
-	// SYSTEM_PROPERTIES : SYSTEM_PROPERTIES,
-	/*
-	 * Publicamos las variables para configurar un almacen de certificados
-	 * concreto.
-	 */
-	// KEYSTORE_WINDOWS : KEYSTORE_WINDOWS,
-	// KEYSTORE_APPLE : KEYSTORE_APPLE,
-	// KEYSTORE_PKCS12 : KEYSTORE_PKCS12,
-	// KEYSTORE_PKCS11 : KEYSTORE_PKCS11,
-	// KEYSTORE_FIREFOX : KEYSTORE_FIREFOX,
-	// KEYSTORE_JAVA : KEYSTORE_JAVA,
-	// KEYSTORE_JCEKS : KEYSTORE_JCEKS,
-	// KEYSTORE_JAVACE : KEYSTORE_JAVACE,
-	/* Metodos visibles. */
-	setCompleteCallback: setCompleteCallback,
-	setBeforeSendCallback: setBeforeSendCallback,
-
-	// cargarMiniApplet : cargarMiniApplet,
-	// cargarAppAfirma : cargarAppAfirma,
-	echo : echo,
-	// checkTime : checkTime,
-	sign : sign,
-	// coSign : coSign,
-	// counterSign : counterSign,
-	// saveDataToFile : saveDataToFile,
-	// getFileNameContentBase64 : getFileNameContentBase64,
-	// getMultiFileNameContentBase64 : getMultiFileNameContentBase64,
-	getBase64FromText : getBase64FromText,
-	// getTextFromBase64 : getTextFromBase64,
-	// setServlets : setServlets,
-	// setStickySignatory : setStickySignatory,
-	// setLocale : setLocale,
-	// getErrorMessage : getErrorMessage,
-	// getErrorType : getErrorType,
-	// getCurrentLog : getCurrentLog
-	openWindow:openWindow,
-	exit: exit_jnlp,
-	test: test
 	};
-})(window, undefined);
+}
+AfirmaClient.prototype.setServer = function(/* String */serverBaseUrl) {
+	this._server = serverBaseUrl;
+}
+AfirmaClient.prototype.setSuccessCallback = function(callback) {
+	this._successCallback = callback;
+}
+AfirmaClient.prototype.setErrorCallback = function(callback) {
+	this._errorCallback = callback;
+}
+AfirmaClient.prototype.setBeforeSendCallback = function(callback) {
+	this._beforeSendCallback = callback;
+}
+AfirmaClient.prototype.setCompleteCallback = function(callback) {
+	this._completeCallback = callback;
+}
+AfirmaClient.prototype.setCommand = function(/* String */command) {
+	this._command = command;
+}
+AfirmaClient.prototype.invoke = function( /* Any */parameters) {
+	this._invoker(this._command, parameters, this._successCallback,
+			this._errorCallback, this._beforeSendCallback,
+			this._completeCallback);
+}
